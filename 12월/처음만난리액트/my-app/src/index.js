9b0044d1538cfc33d33fa1365a6676d9b6f17176
moooonam <anska77@naver.com>
2022-12-14 21:30:22 +0900

    up

--- 12월/처음만난리액트/my-app/src/index.js
+++ 12월/처음만난리액트/my-app/src/index.js
@@ -5,7 +5,8 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 // import Accommodate from './chapter_07/Accommodate';
-import ConfirmButton from './chapter_08/ConfirmButton';
+// import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 // import Library from './chapter_03/Library';
 // import Clock from './chapter_04/Clock';
 // import CommentList from './chapter_05/CommentList';
@@ -22,7 +23,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <LandingPage />
   </React.StrictMode>
 )
 // If you want to start measuring performance in your app, pass a function

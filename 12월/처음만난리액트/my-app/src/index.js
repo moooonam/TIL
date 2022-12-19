6186ee2ef2d3f3b551b44eb60bf3a6c64ab6014d
moooonam <anska77@naver.com>
2022-12-19 11:11:55 +0900

    up

--- 12월/처음만난리액트/my-app/src/index.js
+++ 12월/처음만난리액트/my-app/src/index.js
@@ -6,7 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 // import Accommodate from './chapter_07/Accommodate';
 // import ConfirmButton from './chapter_08/ConfirmButton';
-import LandingPage from './chapter_09/LandingPage';
+// import LandingPage from './chapter_09/LandingPage';
+// import AttendanceBook from './chapter_10/AttendanceBook';
+// import SignUp from './chapter_11/SignUp';
+// import Calculator from './chapter_12/Calculator';
+// import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 // import Library from './chapter_03/Library';
 // import Clock from './chapter_04/Clock';
 // import CommentList from './chapter_05/CommentList';
@@ -23,7 +28,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <DarkOrLight />
   </React.StrictMode>
 )
 // If you want to start measuring performance in your app, pass a function

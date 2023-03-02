86d61914a7bbb29a14eba2e9cdde860db33d5240
SSAFY <anska77@naver.com>
2023-03-02 16:22:07 +0900

    리엑트 정리

--- 2023/2월/voca/src/App.js
+++ 2023/2월/voca/src/App.js
@@ -1,11 +1,28 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import Movie from "./components/Movie";
+import MovieForm from "./components/MovieForm";
 function App() {
-  useEffect(() => {
-    
-  })
+  const [movies, setMovies] = useState([]);
+  const removeMovie = (id) => {
+    setMovies(movies.filter(movie => {
+      return movie.id !== id
+    }))
+  };
+
+  const renderMovies = movies.length
+    ? movies.map((movie) => {
+        return <Movie movie={movie} key={movie.id} removeMovie={removeMovie} />;
+      })
+    : "추가된 영화가 없습니다.";
+
+  const addMovie = (movie) => {
+    setMovies([...movies, movie]);
+  };
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Movie list</h1>
+      <MovieForm addMovie={addMovie} />
+      {renderMovies}
     </div>
   );
 }
